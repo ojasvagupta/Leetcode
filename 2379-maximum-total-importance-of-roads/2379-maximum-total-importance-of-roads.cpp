9631f00880e94e176827329d04ed5380a3505ec3
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-06-28 12:56:28 +0530

    Time: 292 ms (63.28%), Space: 132.7 MB (57.93%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        unordered_map<int, int> mp;
-        vector<vector<int>> v;
-        for (long i = 0; i < n; i++) {
-            mp[i] = 0;
+        long res = 0, cost = 1;
+        vector<int> conn(n, 0);
+        for (auto road : roads) {
+            conn[road[0]]++;
+            conn[road[1]]++;
         }
-        for (long i = 0; i < roads.size(); i++) {
-            long a = roads[i][0];
-            long b = roads[i][1];
-            mp[a]++;
-            mp[b]++;
-        }
-        for (auto a : mp) {
-            v.push_back({a.second, a.first});
-        }
-        sort(v.begin(), v.end());
-        long long sum = 0;
-        for (long i = 1; i <= v.size(); i++) {
-            sum = sum + i * v[i - 1][0];
-        }
-        return sum;
+        sort(conn.begin(), conn.end());
+        for (auto con : conn) res += con * (cost++);
+        return res;
     }
 };
