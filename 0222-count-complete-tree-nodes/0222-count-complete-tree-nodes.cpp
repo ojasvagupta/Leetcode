65e53eee80f44104254cead0852b7378efe1a057
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-07-04 14:13:25 +0530

    Time: 33 ms (5.96%), Space: 29.1 MB (46.06%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -6,17 +6,16 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        if(root==NULL)
-        {
+        if (root == NULL) {
             return NULL;
         }
-        return countNodes(root->left) + countNodes(root->right) +1;
-        
+        return countNodes(root->left) + countNodes(root->right) + 1;
     }
 };
