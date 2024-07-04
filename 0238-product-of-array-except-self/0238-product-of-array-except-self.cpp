26c7c2feb16c1c8bcb04689037ff5c591aced72e
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-07-04 14:12:20 +0530

    Time: 19 ms (95.31%), Space: 43.4 MB (6.14%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -9,10 +9,10 @@ public:
             v.push_back(left);
             left = left * nums[i];
         }
-        int right = 1;
+        left = 1;
         for (int i = n - 1; i >= 0; i--) {
-            w.push_back(right);
-            right = right * nums[i];
+            w.push_back(left);
+            left = left * nums[i];
         }
         for (int i = 0; i < n; i++) {
             nums[i] = v[i] * w[n - 1 - i];
