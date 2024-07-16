b280c9225877633aed04428c0ab0505281458b8d
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-07-16 11:52:20 +0530

    Time: 144 ms (85.78%), Space: 121.9 MB (27.68%) - LeetHub

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -1,62 +1,45 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
+
+private:
+    void rootToNode(TreeNode* root,int &n,int &m,string &temp,string &s,string &d){
+        if(!root) return;
+
+        if(root->val == n) s=temp;
+        if(root->val == m) d=temp;
+        
+        temp.push_back('L');
+        rootToNode(root->left, n, m, temp, s, d);
+        temp.pop_back();
+
+        temp.push_back('R');
+        rootToNode(root->right, n, m, temp, s, d);
+        temp.pop_back();
+    }
+
+
 public:
-    TreeNode* findLCA(TreeNode* root, int p, int q) {
-        if (root == nullptr || root->val == p || root->val == q) {
-            return root;
-        }
-        TreeNode* left = findLCA(root->left, p, q);
-        TreeNode* right = findLCA(root->right, p, q);
-        if (left != nullptr && right != nullptr) {
-            return root;
-        }
-        return left != nullptr ? left : right;
-    }
+    string getDirections(TreeNode* root, int n, int m) {
 
-    bool findPath(TreeNode* root, int value, string& path) {
-        if (root == nullptr) {
-            return false;
-        }
-        if (root->val == value) {
-            return true;
-        }
-        path.push_back('L');
-        if (findPath(root->left, value, path)) {
-            return true;
-        }
-        path.pop_back();
-        
-        path.push_back('R');
-        if (findPath(root->right, value, path)) {
-            return true;
-        }
-        path.pop_back();
-        
-        return false;
-    }
+        string s,d,temp;
+        rootToNode(root,n,m,temp,s,d);
 
-    string getDirections(TreeNode* root, int startValue, int destValue) {
-        TreeNode* lca = findLCA(root, startValue, destValue);
-        
-        string pathToStart = "";
-        string pathToDest = "";
-        
-        findPath(lca, startValue, pathToStart);
-        findPath(lca, destValue, pathToDest);
-        for (char& c : pathToStart) {
-            c = 'U';
+        int ind = 0;
+
+        for(int i=0;i<s.size() && i<d.size();i++){
+            if(s[i]==d[i]) ind=i+1;
+            else break;
         }
-        
-        return pathToStart + pathToDest;
+
+        string ans;
+
+        for(int i = ind;i<s.size();i++){
+            ans.push_back('U');
+        }
+
+        for(int i = ind;i<d.size();i++){
+            ans.push_back(d[i]);
+        }
+
+        return ans;
     }
 };
