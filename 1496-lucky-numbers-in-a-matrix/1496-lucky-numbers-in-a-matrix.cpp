1bb606ff59215c877be01e69e670603e3d483fd5
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-07-19 20:04:43 +0530

    Time: 16 ms (56.65%), Space: 14.3 MB (18.75%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
-        
+    vector<int> luckyNumbers(vector<vector<int>>& matrix) {
+
         vector<int> res;
-        for(int i=0;i<matrix.size();i++)
-        {
-            int temp1=INT_MAX;
-            int temp2=INT_MIN;
-            int check=0;
-            for(int j=0;j<matrix[i].size();j++)
-            {
-                temp1=min(temp1,matrix[i][j]);
+        for (int i = 0; i < matrix.size(); i++) {
+            int temp1 = INT_MAX;
+            int temp2 = INT_MIN;
+            int check = 0;
+            for (int j = 0; j < matrix[i].size(); j++) {
+                temp1 = min(temp1, matrix[i][j]);
             }
-            for(int j=0;j<matrix[i].size();j++)
-            {
-                
-                if(temp1==matrix[i][j])
-                {
-                    check=j;
+            for (int j = 0; j < matrix[i].size(); j++) {
+
+                if (temp1 == matrix[i][j]) {
+                    check = j;
                 }
             }
-            for(int j=0;j<matrix.size();j++)
-            {
-                temp2=max(temp2,matrix[j][check]);
+            for (int j = 0; j < matrix.size(); j++) {
+                temp2 = max(temp2, matrix[j][check]);
             }
-            if(temp1==temp2)
-            {
+            if (temp1 == temp2) {
                 res.push_back(temp1);
             }
         }
         return res;
-        
     }
 };
