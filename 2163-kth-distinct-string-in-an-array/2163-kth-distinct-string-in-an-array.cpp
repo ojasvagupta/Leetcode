6444feae508123882ee95455f6417d6f3908b89d
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-08-05 18:34:30 +0530

    Time: 20 ms (36.87%), Space: 18.4 MB (42.71%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.cpp
@@ -1,45 +1,19 @@
 class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
-        unordered_map<string,vector<int>> mp;
-        for(int i=0;i<arr.size();i++)
-        {
-            if(mp.count(arr[i]))
-            {
-                mp[arr[i]][0]++;
-            }
-            else
-            {
-                mp[arr[i]].push_back(1);
-                mp[arr[i]].push_back(i);
-            }
+        map<string, int> mp;
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            mp[arr[i]]++;
         }
-        vector<string> v;
-        for(auto it=mp.begin();it!=mp.end();it++)
-        {
-            auto a=it->second;
-            if(a[0]==1)
-            {
-                v.push_back(it->first);
-            }
-        }
-        int count=0;
-        for(int i=0;i<arr.size();i++)
-        {
-            for(int j=0;j<v.size();j++)
-            {
-                if(arr[i]==v[j])
-                {
-                    count++;
-                    break;
-                }
-            }
-            if(count==k)
-            {
-                return arr[i];
+        int count = 0;
+        for (auto n : arr) {
+            if (mp[n] == 1) {
+                count++;
+                if (count == k)
+                    return n;
             }
         }
         return "";
-        
     }
 };
