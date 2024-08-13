63d423f1e1200229a79719af55e0d3f3d7c093cc
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-08-13 18:23:42 +0530

    Time: 22 ms (10.55%), Space: 19.3 MB (8.01%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -10,20 +10,19 @@ public:
             return;
         }
         for (int j = i; j < candidates.size(); j++) {
-            
+
             if (j > i && candidates[j] == candidates[j - 1])
                 continue;
-            
+
             temp.push_back(candidates[j]);
             helper(candidates, target, j + 1, currsum + candidates[j], temp);
             temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        ans.clear(); 
+        ans.clear();
         vector<int> temp;
-        sort(candidates.begin(),
-             candidates.end()); 
+        sort(candidates.begin(), candidates.end());
         helper(candidates, target, 0, 0, temp);
         return ans;
     }
