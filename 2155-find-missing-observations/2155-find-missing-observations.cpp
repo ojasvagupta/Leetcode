86add6b9754ea235d1d8c7aa7f7ccbe386cec749
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-09-05 23:39:37 +0530

    Time: 102 ms (42.16%), Space: 118.1 MB (43.63%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.cpp
+++ 2155-find-missing-observations/2155-find-missing-observations.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
-        vector<int> v(n,1);
-        int sum1=0;
-        for(int i=0;i<rolls.size();i++)
-        {
-            sum1=sum1+rolls[i];
+        vector<int> v(n, 1);
+        int sum1 = 0;
+        for (int i = 0; i < rolls.size(); i++) {
+            sum1 = sum1 + rolls[i];
         }
-        int sum2=mean*(rolls.size()+n)-sum1;
-        if(sum2 < n || sum2 > 6 * n) {
-            return {}; 
+        int sum2 = mean * (rolls.size() + n) - sum1;
+        if (sum2 < n || sum2 > 6 * n) {
+            return {};
         }
-        int diff=sum2-n;
-        int c=0;
-        while(diff>0)
-        {
-           if(c==n)
-           {
-            c=0;
-           }
-           v[c]++;
+        int diff = sum2 - n;
+        int c = 0;
+        while (diff > 0) {
+            if (c == n) {
+                c = 0;
+            }
+            v[c]++;
             diff--;
             c++;
         }
-        
+
         return v;
-        
     }
 };
