b18e0c422990f18db32ccc14f57eb49026023346
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-09-09 21:09:53 +0530

    Time: 180 ms (14.19%), Space: 130.6 MB (65.45%) - LeetHub

---
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        if(m==1 && n==1)
+        {
+            return {{head->val}};
+        }
+        vector<vector<int>> v(m, vector<int>(n, -1));
+        int left = 0;
+        int right = n - 1;
+        int top = 0;
+        int bottom = m - 1;
+        ListNode* root = head;
+        while (left<=right && top<=bottom && root != nullptr) {
+            
+            for (int i = left; i <= right; i++) {
+                 if (root == nullptr) {
+                    break;
+                }
+                v[top][i] = root->val;
+                root = root->next;
+               
+                
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                if (root == nullptr) {
+                    break;
+                }
+                v[i][right] = root->val;
+                root = root->next;
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                if (root == nullptr) {
+                    break;
+                }
+                v[bottom][i] = root->val;
+                root = root->next;
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                if (root == nullptr) {
+                    break;
+                }
+                v[i][left] = root->val;
+                root = root->next;
+            }
+            left++;
+            
+        }
+        return v;
+    }
+};
