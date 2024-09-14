22135c77e35503af18d3cb9a51eecb0da9b76d43
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-09-14 12:33:34 +0530

    Time: 104 ms (62.28%), Space: 85 MB (34.21%) - LeetHub

---
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxi=INT_MIN;
+        for(auto a : nums)
+        {
+            if(a>maxi)
+            {
+                maxi=max(maxi,a);
+            }
+        }
+        int i=0;
+        int count=0;
+        int res=0;
+        while(i<nums.size())
+        {
+            if(nums[i]==maxi)
+            {
+                count++;
+            }
+            else
+            {
+                count=0;
+            }
+            res=max(res,count);
+            i++;
+        }
+        return res;
+    }
+};
