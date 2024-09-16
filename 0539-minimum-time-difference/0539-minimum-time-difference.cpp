0ef7b7924e943165945e3e3417a0174ae8abbee5
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-09-16 21:34:23 +0530

    Time: 11 ms (69.95%), Space: 17.5 MB (77.18%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -2,26 +2,23 @@ class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
         vector<int> v;
-        for(int i=0;i<timePoints.size();i++)
-        {
-            
-            string a=timePoints[i].substr(0,2);
-            int hours=stoi(a);
-            a=timePoints[i].substr(3,2);
-            int min=(hours)*60 + stoi(a);
-           
+        for (int i = 0; i < timePoints.size(); i++) {
+
+            string a = timePoints[i].substr(0, 2);
+            int hours = stoi(a);
+            a = timePoints[i].substr(3, 2);
+            int min = (hours) * 60 + stoi(a);
+
             v.push_back(min);
         }
-        sort(v.begin(),v.end());
-        int res=INT_MAX;
-        int diff=1440-v.back()+v[0];
-        res=min(res,diff);
-        for(int i=1;i<v.size();i++)
-        {
-            diff=(v[i]-v[i-1]);
-            res=min(res,diff);
+        sort(v.begin(), v.end());
+        int res = INT_MAX;
+        int diff = 1440 - v.back() + v[0];
+        res = min(res, diff);
+        for (int i = 1; i < v.size(); i++) {
+            diff = (v[i] - v[i - 1]);
+            res = min(res, diff);
         }
-        
 
         return res;
     }
