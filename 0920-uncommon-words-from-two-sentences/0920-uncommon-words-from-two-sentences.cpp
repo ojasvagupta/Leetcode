b41e9a445a2280ae1ed7399cd524360ebb525f6c
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-09-17 12:36:31 +0530

    Time: 0 ms (100%), Space: 9.8 MB (5.3%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -1,49 +1,40 @@
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
-        unordered_map<string,int> mp;
-        unordered_map<string,int> mp1;
+        unordered_map<string, int> mp;
+        unordered_map<string, int> mp1;
         vector<string> res;
-        int i=0;
-        while(i<s1.size())
-        {
-            string a="";
-            while(i<s1.size() && s1[i]!=' ')
-            {
-                a=a+s1[i];
+        int i = 0;
+        while (i < s1.size()) {
+            string a = "";
+            while (i < s1.size() && s1[i] != ' ') {
+                a = a + s1[i];
                 i++;
             }
             mp[a]++;
             i++;
         }
-        i=0;
-        while(i<s2.size())
-        {
-            string a="";
-            while(i<s2.size() && s2[i]!=' ')
-            {
-                a=a+s2[i];
+        i = 0;
+        while (i < s2.size()) {
+            string a = "";
+            while (i < s2.size() && s2[i] != ' ') {
+                a = a + s2[i];
                 i++;
             }
             mp1[a]++;
-            if(mp.count(a))
-            {
-                mp[a]=0;
-                mp1[a]=0;
+            if (mp.count(a)) {
+                mp[a] = 0;
+                mp1[a] = 0;
             }
             i++;
         }
-        for(auto a : mp)
-        {
-            if(a.second==1)
-            {
+        for (auto a : mp) {
+            if (a.second == 1) {
                 res.push_back(a.first);
             }
         }
-        for(auto a : mp1)
-        {
-            if(a.second==1)
-            {
+        for (auto a : mp1) {
+            if (a.second == 1) {
                 res.push_back(a.first);
             }
         }
