ae9ca748d7322f51368e58841cd53250271378ee
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-09-21 00:11:39 +0530

    Time: 3 ms (95.98%), Space: 11.7 MB (38.92%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        if (s.empty()) return s;
+        if (s.empty())
+            return s;
 
         // Create the reverse of the string s
         string rev = s;
         reverse(rev.begin(), rev.end());
-        
+
         // Concatenate original string and reversed string with a separator
         string combined = s + "#" + rev;
-        
-        // KMP table to store the length of the longest prefix which is also a suffix
+
+        // KMP table to store the length of the longest prefix which is also a
+        // suffix
         vector<int> kmp(combined.size(), 0);
-        
+
         for (int i = 1; i < combined.size(); i++) {
             int j = kmp[i - 1];
-            
+
             while (j > 0 && combined[i] != combined[j]) {
                 j = kmp[j - 1];
             }
-            
+
             if (combined[i] == combined[j]) {
                 j++;
             }
-            
+
             kmp[i] = j;
         }
-        
+
         // The length of the longest palindromic prefix
         int longest_palindrome_prefix_len = kmp.back();
-        
-        // Add the non-palindrome part of the reversed string in front of the original string
+
+        // Add the non-palindrome part of the reversed string in front of the
+        // original string
         return rev.substr(0, s.size() - longest_palindrome_prefix_len) + s;
     }
 };
