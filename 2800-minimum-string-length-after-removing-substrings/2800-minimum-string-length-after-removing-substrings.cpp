c7db2d6a57aa557160bbc8d0e5fdf907012972ce
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-10-07 13:45:16 +0530

    Time: 27 ms (5.05%), Space: 15.1 MB (5.05%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int minLength(string s) {
-        bool count=false;
-        int i=0;
-        while(!count)
-        {
-            if(s.substr(i,2)=="AB" or s.substr(i,2)=="CD")
-            {
-                s=s.substr(0,i)+s.substr(i+2);
-                i=0;
-            }
-            else
-            {
+        bool count = false;
+        int i = 0;
+        while (!count) {
+            if (s.substr(i, 2) == "AB" or s.substr(i, 2) == "CD") {
+                s = s.substr(0, i) + s.substr(i + 2);
+                if (i != 0) {
+                    i = i - 1;
+                }
+            } else {
                 i++;
             }
-            if(i>s.size())
-            {
-                count=true;
+            if (i > s.size()) {
+                count = true;
             }
         }
         return s.size();
-        
     }
 };
