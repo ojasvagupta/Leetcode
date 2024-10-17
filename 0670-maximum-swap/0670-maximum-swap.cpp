efffff836ea82be187066bb5ca41ca96715eb6d9
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-10-17 15:12:26 +0530

    Time: 3 ms (38.18%), Space: 7.8 MB (12.29%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string s = to_string(num);
+        bool sp = false;
+        int i = 0;
+        int j = 0;
+        while (!sp && j<s.size()) {
+            stack<pair<char, int>> st;
+            st.push({s[i], i});
+            while (i < s.size()) {
+                auto x = st.top();
+                if (s[i] >= x.first) {
+                    st.push({s[i], i});
+                }
+                i++;
+            }
+            auto x = st.top();
+            if (x.first== s[j]) {
+                while (s[j] == x.first) {
+                    j++;
+                }
+            } else if (x.first > s[j]) {
+                swap(s[x.second], s[j]);
+                sp=true;
+            }
+            i=j;
+            
+        }
+        int res=stoi(s);
+        return res;
+    }
+};
