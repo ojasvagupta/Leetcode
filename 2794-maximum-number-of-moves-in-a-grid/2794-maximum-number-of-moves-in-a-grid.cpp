42807e16c8a2478fafd2c264ff733dc1d553a001
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-10-29 21:36:06 +0530

    Time: 11 ms (78.26%), Space: 74.5 MB (25.57%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-void count(vector<vector<int>>& grid,int i,int j,int nums,int cnt,int& res,vector<vector<int>>& memo)
-{
-    if(i<0 or i>=grid.size() or j<0 or j>=grid[0].size() or grid[i][j]<=nums)
-    {
-        return;
+    void count(vector<vector<int>>& grid, int i, int j, int nums, int cnt,
+               int& res, vector<vector<int>>& memo) {
+        if (i < 0 or i >= grid.size() or j < 0 or j >= grid[0].size() or
+            grid[i][j] <= nums) {
+            return;
+        }
+        if (memo[i][j] != 0) {
+            cnt = memo[i][j];
+            res = max(cnt, res);
+            return;
+        }
+        nums = grid[i][j];
+        count(grid, i - 1, j + 1, nums, cnt + 1, res, memo);
+        count(grid, i, j + 1, nums, cnt + 1, res, memo);
+        count(grid, i + 1, j + 1, nums, cnt + 1, res, memo);
+        res = max(res, cnt);
+        memo[i][j] = res;
     }
-    if(memo[i][j]!=0)
-    {
-        cnt=memo[i][j];
-        res=max(cnt,res);
-        return;
-    }
-    nums=grid[i][j];
-    count(grid,i-1,j+1,nums,cnt+1,res,memo);
-    count(grid,i,j+1,nums,cnt+1,res,memo);
-    count(grid,i+1,j+1,nums,cnt+1,res,memo);
-    res=max(res,cnt);
-    memo[i][j]=res;
-}
     int maxMoves(vector<vector<int>>& grid) {
-        int res=0;
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>> memo(n,vector<int>(m,0));
-        for(int i=0;i<grid.size();i++)
-        {
-            int cnt=0;
-            count(grid,i,0,-1,cnt,res,memo);
-            res=max(res,cnt);
+        int res = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> memo(n, vector<int>(m, 0));
+        for (int i = 0; i < grid.size(); i++) {
+            int cnt = 0;
+            count(grid, i, 0, -1, cnt, res, memo);
+            res = max(res, cnt);
         }
         return res;
-        
     }
 };
