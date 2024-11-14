017335c46c564fd641e130db4c0a21473ba46021
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-11-15 01:09:28 +0530

    Time: 39 ms (63.24%), Space: 87.3 MB (34.03%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int minimizedMaximum(int n, vector<int>& quantities) {
-        int left = 1, right = *max_element(quantities.begin(), quantities.end());
+        int left = 1,
+            right = *max_element(quantities.begin(), quantities.end());
         int res = right;
-        
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
             int requiredStores = 0;
-            
+
             // Calculate the number of stores needed if max per store is mid
             for (int quantity : quantities) {
-                requiredStores += (quantity + mid - 1) / mid;  // Equivalent to ceil(quantity / mid)
+                requiredStores += (quantity + mid - 1) /
+                                  mid; // Equivalent to ceil(quantity / mid)
             }
-            
+
             if (requiredStores <= n) {
-                res = mid;  // Try to minimize the maximum value
+                res = mid; // Try to minimize the maximum value
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        
+
         return res;
     }
 };
