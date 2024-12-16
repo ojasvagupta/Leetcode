4465943d522f0e70b1bddb7e1fa190b9653e9ca0
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-12-16 16:34:55 +0530

    Time: 0 ms (100%), Space: 27.9 MB (20.77%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        // Custom comparator for priority_queue
-        auto comparator = [](const pair<int, int>& a, const pair<int, int>& b) {
-            if (a.first == b.first) {
-                return a.second > b.second;
-            }
-            return a.first > b.first; // Min-heap based on the first element
-        };
+        int n = nums.size();
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       decltype(comparator)>
-            pq(comparator);
-
-        // Push the first k elements into the priority queue
-        for (int i = 0; i < nums.size(); i++) {
-            pq.push({nums[i], i});
+        for(int i=0;i<n;i++){
+            minHeap.push({nums[i],i});
         }
 
-        // Process the priority queue for k iterations
-        for (int i = 0; i < k; i++) {
-            auto [a, b] = pq.top();
-            pq.pop();
-            a = a * multiplier;
-            nums[b] = nums[b] * multiplier;
-            pq.push({a, b});
+        while(k){
+            auto [value,index] = minHeap.top();
+            nums[index] = nums[index]*multiplier;
+            minHeap.pop();
+            minHeap.push({nums[index],index});
+            k--;
         }
-
         return nums;
     }
 };
