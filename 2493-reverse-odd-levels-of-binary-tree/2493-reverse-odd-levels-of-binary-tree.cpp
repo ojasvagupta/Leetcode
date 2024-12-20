401b768ab3552326974076ee7c9fb1b2a40f6f19
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-12-20 16:54:52 +0530

    Time: 0 ms (100%), Space: 84.1 MB (13.13%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
-        if (!root) return root;
+        if (!root)
+            return root;
 
         queue<TreeNode*> q;
         q.push(root);
@@ -18,8 +19,10 @@ public:
 
                 if (node) {
                     levelNodes.push_back(node);
-                    if (node->left) q.push(node->left);
-                    if (node->right) q.push(node->right);
+                    if (node->left)
+                        q.push(node->left);
+                    if (node->right)
+                        q.push(node->right);
                 }
             }
 
