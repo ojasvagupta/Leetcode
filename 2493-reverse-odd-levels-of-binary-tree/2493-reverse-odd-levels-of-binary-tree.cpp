7b87fb90fb1483dcf3e742f0b6fcc506e3b35093
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2024-12-20 16:54:40 +0530

    Time: 4 ms (42.65%), Space: 84.2 MB (13.13%) - LeetHub

---
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        if (!root) return root;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        bool isOddLevel = false;
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<TreeNode*> levelNodes;
+
+            // Collect nodes at the current level
+            for (int i = 0; i < size; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if (node) {
+                    levelNodes.push_back(node);
+                    if (node->left) q.push(node->left);
+                    if (node->right) q.push(node->right);
+                }
+            }
+
+            // Reverse values if it's an odd level
+            if (isOddLevel) {
+                int left = 0, right = levelNodes.size() - 1;
+                while (left < right) {
+                    swap(levelNodes[left]->val, levelNodes[right]->val);
+                    ++left;
+                    --right;
+                }
+            }
+
+            isOddLevel = !isOddLevel; // Toggle the level indicator
+        }
+
+        return root;
+    }
+};
