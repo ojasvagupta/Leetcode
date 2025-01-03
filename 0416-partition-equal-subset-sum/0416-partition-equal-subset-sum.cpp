7a6a9841c58e1d585efa0680987c6f5edeae43a0
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-01-04 00:07:57 +0530

    Time: 191 ms (27.49%), Space: 346.9 MB (5.11%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    bool clc(vector<int>& nums, int sum, int total, int i,vector<vector<int>>& memo) {
-        if(sum==total)
-        {
+    bool clc(vector<int>& nums, int sum, int total, int i,
+             vector<vector<int>>& memo) {
+        if (sum == total) {
             return true;
         }
-        if(i<0)
-        {
+        if (i < 0) {
             return false;
         }
-        if(memo[i][sum]!=-1)
-        {
+        if (memo[i][sum] != -1) {
             return memo[i][sum];
         }
         bool res = false;
-        res = clc(nums, sum, total, i - 1,memo);
-        res = res or clc(nums, sum + nums[i], total - nums[i], i - 1,memo);
-        if(res)
-        {
-            memo[i][sum]=1;
-        }
-        else
-        {
-            memo[i][sum]=0;
+        res = clc(nums, sum, total, i - 1, memo);
+        res = res or clc(nums, sum + nums[i], total, i - 1, memo);
+        if (res) {
+            memo[i][sum] = 1;
+        } else {
+            memo[i][sum] = 0;
         }
         return res;
     }
     bool canPartition(vector<int>& nums) {
         int total = 0;
-        vector<vector<int>> memo(nums.size(),vector<int> (20001,-1));
+        vector<vector<int>> memo(nums.size(), vector<int>(20001, -1));
         for (auto a : nums) {
             total += a;
         }
-        return clc(nums, 0, total, nums.size()-1,memo);
+        if (total % 2 != 0) {
+            return false;
+        }
+        else
+        {
+            total=total/2;
+        }
+        return clc(nums, 0, total, nums.size() - 1, memo);
     }
 };
