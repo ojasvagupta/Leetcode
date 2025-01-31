d408ad4f49c9888aecf531bc283d9acf6e10a975
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-01-31 23:52:26 +0530

    Time: 619 ms (16.41%), Space: 312.1 MB (11.58%) - LeetHub

--- 0854-making-a-large-island/0854-making-a-large-island.cpp
+++ 0854-making-a-large-island/0854-making-a-large-island.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int clc(vector<vector<int>>& grid, int i, int j,
-            vector<vector<bool>>& vis,int islandID) {
+    int clc(vector<vector<int>>& grid, int i, int j, vector<vector<bool>>& vis,
+            int islandID) {
 
         if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) {
             return 0;
@@ -13,65 +13,57 @@ public:
         if (grid[i][j] == 0) {
             return 0;
         }
-        grid[i][j]=islandID;
+        grid[i][j] = islandID;
         int res = 1;
-        res = res + clc(grid, i + 1, j, vis ,islandID);
-        res = res + clc(grid, i, j + 1, vis ,islandID);
-        res = res + clc(grid, i - 1, j, vis ,islandID);
-        res = res + clc(grid, i, j - 1, vis ,islandID);
+        res = res + clc(grid, i + 1, j, vis, islandID);
+        res = res + clc(grid, i, j + 1, vis, islandID);
+        res = res + clc(grid, i - 1, j, vis, islandID);
+        res = res + clc(grid, i, j - 1, vis, islandID);
         return res;
     }
     int largestIsland(vector<vector<int>>& grid) {
-        int islandID=2;
-        unordered_map<int,int> mp;
-        vector<vector<bool>> vis(
-                        grid.size(), vector<bool>(grid[0].size(), false));
+        int islandID = 2;
+        unordered_map<int, int> mp;
+        vector<vector<bool>> vis(grid.size(),
+                                 vector<bool>(grid[0].size(), false));
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == 1 && !vis[i][j]) {
-                    int val = clc(grid, i, j, vis,islandID);
-                    mp[islandID]=val;
+                    int val = clc(grid, i, j, vis, islandID);
+                    mp[islandID] = val;
                     islandID++;
                 }
             }
         }
-        int res=0;
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0;j<grid[0].size();j++)
-            {
-                if(grid[i][j]==0)
-                {
+        int res = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 0) {
                     unordered_set<int> st;
-                    int temp=1;
-                    if(i+1<grid.size())
-                    {
-                        temp=temp+mp[grid[i+1][j]];
-                        st.insert(grid[i+1][j]);
+                    int temp = 1;
+                    if (i + 1 < grid.size()) {
+                        temp = temp + mp[grid[i + 1][j]];
+                        st.insert(grid[i + 1][j]);
                     }
-                    if(j+1<grid[0].size() && !st.count(grid[i][j+1]))
-                    {
-                        temp=temp+mp[grid[i][j+1]];
-                        st.insert(grid[i][j+1]);
+                    if (j + 1 < grid[0].size() && !st.count(grid[i][j + 1])) {
+                        temp = temp + mp[grid[i][j + 1]];
+                        st.insert(grid[i][j + 1]);
                     }
-                    if(i-1>=0 && !st.count(grid[i-1][j]))
-                    {
-                        temp=temp+mp[grid[i-1][j]];
-                        st.insert(grid[i-1][j]);
+                    if (i - 1 >= 0 && !st.count(grid[i - 1][j])) {
+                        temp = temp + mp[grid[i - 1][j]];
+                        st.insert(grid[i - 1][j]);
                     }
-                    if(j-1>=0 && !st.count(grid[i][j-1]) )
-                    {
-                        temp=temp+mp[grid[i][j-1]];
+                    if (j - 1 >= 0 && !st.count(grid[i][j - 1])) {
+                        temp = temp + mp[grid[i][j - 1]];
                     }
-                    res=max(res,temp);
+                    res = max(res, temp);
                 }
             }
         }
-        if(res==0)
-        {
-            return grid[0].size()*grid.size();
+        if (res == 0) {
+            return grid[0].size() * grid.size();
         }
-        
+
         return res;
     }
 };
