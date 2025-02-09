f96dc34c885deae9a43b3a6ef459caf86607456d
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-02-09 17:14:54 +0530

    Time: 77 ms (26.29%), Space: 87.5 MB (87.39%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
-        long long res=0;
-        long long count=1;
-        unordered_map<int,int> mp;
-        nums[0]=-nums[0];
-        mp[nums[0]]=1;
-        for(int i=1;i<nums.size();i++)
-        {
-            nums[i]=i-nums[i];
-            if(!mp.count(nums[i]))
-            {
-                mp[nums[i]]=1;
-                res=res+count;
+        long long res = 0;
+        long long count = 1;
+        unordered_map<int, int> mp;
+        nums[0] = -nums[0];
+        mp[nums[0]] = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            nums[i] = i - nums[i];
+            if (!mp.count(nums[i])) {
+                mp[nums[i]] = 1;
+                res = res + count;
                 count++;
-            }
-            else
-            {
-                res=res+count-mp[nums[i]];
+            } else {
+                res = res + count - mp[nums[i]];
                 count++;
                 mp[nums[i]]++;
             }
         }
         return res;
-
     }
 };
