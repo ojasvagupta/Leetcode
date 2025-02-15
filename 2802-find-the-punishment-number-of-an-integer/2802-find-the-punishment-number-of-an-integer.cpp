d31db8ad3bcb25ffe23566d204b83cdf0c75b7c4
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-02-15 23:17:16 +0530

    Time: 11 ms (77.58%), Space: 7.9 MB (95.72%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-bool check(int pro,int sum,int& prev)
-{
-    if(sum==prev && pro==0)
-    {
-        return true;
+    bool check(int pro, int sum, int& prev) {
+        if (sum == prev && pro == 0) {
+            return true;
+        }
+        int i = 10;
+        bool res = false;
+        int mul = pro;
+        while (mul > 0) {
+            int temp = pro % i;
+            mul = pro / i;
+            res = res || check(mul, sum + temp, prev);
+            i = i * 10;
+        }
+        return res;
     }
-    int i=10;
-    bool res=false;
-    int mul=pro;
-    while(mul>0)
-    {
-        int temp=pro%i;
-        mul=pro/i;
-        res=res || check(mul,sum+temp,prev);
-        i=i*10;
-    }
-    return res;
-
-}
     int punishmentNumber(int n) {
-        int res=0;
-        for(int i=1;i<=n;i++)
-        {
-            int pro=i*i;
-            if(check(pro,0,i))
-            {
-                res=res+pro;
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            int pro = i * i;
+            if (check(pro, 0, i)) {
+                res = res + pro;
             }
         }
         return res;
