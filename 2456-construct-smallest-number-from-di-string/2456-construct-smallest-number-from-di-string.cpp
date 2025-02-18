f9b9515d501caf8c21f0e2e24e55411e342889a5
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-02-18 22:33:57 +0530

    Time: 3 ms (26.77%), Space: 10.2 MB (5.27%) - LeetHub

---
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    void clc(int a, string& pattern, string s, unordered_set<int>& st,
+             bool& check, string& res) {
+        if (check) {
+            return;
+        }
+        if (a >= pattern.length()) {
+            res = s;
+            check = true;
+            return;
+        }
+        for (int j = 1; j < 10; j++) {
+            if (pattern[a] == 'I' && j > (s[a]-'0') && !st.count(j)) {
+                st.insert(j);
+                clc(a + 1, pattern, s + to_string(j), st, check, res);
+                st.erase(j);
+            }
+            if (pattern[a] == 'D' && j < (s[a]-'0') && !st.count(j)) {
+                st.insert(j);
+                clc(a + 1, pattern, s + to_string(j), st, check, res);
+                st.erase(j);
+            }
+        }
+    }
+    string smallestNumber(string pattern) {
+        string res = "";
+        unordered_set<int> st;
+        int k;
+        if (pattern[0] == 'D') {
+            k = INT_MAX;
+        } else {
+            k = 0;
+        }
+        bool check = false;
+        for(int i=1;i<10;i++)
+        {
+            st.insert(i);
+            clc(0, pattern, to_string(i), st, check, res);
+            st.erase(i);
+        if (check) {
+            return res;
+        }
+        }
+        
+        return res;
+    }
+};
