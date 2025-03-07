e13af6d922a42860fddd35aaa3848680185881a7
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-03-08 00:07:10 +0530

    Time: 1 ms (94.44%), Space: 8.1 MB (96.48%) - LeetHub

---
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    // Function to check if a number is prime
+    bool check(int val) {
+        if (val < 2) return false; // 0 and 1 are not prime
+        for (int i = 2; i * i <= val; i++) {
+            if (val % i == 0) return false;
+        }
+        return true;
+    }
+
+    vector<int> closestPrimes(int left, int right) {
+        vector<int> v(2, -1);
+        int diff = INT_MAX;
+        int first = -1;
+        
+        for (int i = left; i <= right; i++) {
+            if (check(i)) {
+                if (first == -1) {
+                    first = i;
+                } else {
+                    int second = i;
+                    if (second - first < diff) {
+                        v[0] = first;
+                        v[1] = second;
+                        diff = second - first;
+                        if (diff <= 2) return v; // Early exit for closest primes
+                    }
+                    first = second; // Update first for the next iteration
+                }
+            }
+        }
+
+        return v;
+    }
+};
