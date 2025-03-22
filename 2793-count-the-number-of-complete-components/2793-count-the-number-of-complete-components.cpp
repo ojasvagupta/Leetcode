fa4017a05a2d4cd03b667c47cf6c0f2e1899747c
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-03-22 17:06:44 +0530

    Time: 89 ms (22.15%), Space: 139.3 MB (24.83%) - LeetHub

---
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void dfs(int node, unordered_map<int, vector<int>>& mp, unordered_set<int>& vis, vector<int>& component) {
+        vis.insert(node);
+        component.push_back(node);
+        for (int neighbor : mp[node]) {
+            if (!vis.count(neighbor)) {
+                dfs(neighbor, mp, vis, component);
+            }
+        }
+    }
+
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> mp;
+        for (auto& edge : edges) {
+            mp[edge[0]].push_back(edge[1]);
+            mp[edge[1]].push_back(edge[0]);
+        }
+
+        unordered_set<int> vis;
+        int res = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (!vis.count(i)) {
+                vector<int> component;
+                dfs(i, mp, vis, component);
+                int compSize = component.size();
+                bool isComplete = true;
+
+                for (int node : component) {
+                    if (mp[node].size() != compSize - 1) {
+                        isComplete = false;
+                        break;
+                    }
+                }
+
+                if (isComplete) res++;
+            }
+        }
+
+        return res;
+    }
+};
