8df19685d0d4a1ddde5582dfc9e11a4568f651eb
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-03-26 22:29:30 +0530

    Time: 47 ms (42.86%), Space: 90.2 MB (20.97%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        vector<int> nums;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                nums.push_back(grid[i][j]);
+        vector<int> values;
+        for (const auto& row : grid) {
+            for (int val : row) {
+                values.push_back(val);
             }
         }
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int mead = nums[n/2];
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            if ((nums[i] - nums[0]) % x != 0) {
+
+        sort(values.begin(), values.end());
+
+        for (int val : values) {
+            if (abs(val - values[0]) % x != 0) {
                 return -1;
             }
-            res = res + abs(mead - nums[i]) / x;
         }
-        return res;
+
+        int median = values[values.size() / 2];
+        int operations = 0;
+
+        for (int val : values) {
+            operations += abs(val - median) / x;
+        }
+
+        return operations;
     }
 };
