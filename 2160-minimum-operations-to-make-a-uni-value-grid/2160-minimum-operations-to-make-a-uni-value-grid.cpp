28eb4c068827776bbbcd8bc1f6e2dab7d3c917a5
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-03-26 22:18:47 +0530

    Time: 56 ms (19.76%), Space: 90 MB (62.01%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -2,33 +2,26 @@ class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
         vector<int> nums;
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0;j<grid[0].size();j++)
-            {
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
                 nums.push_back(grid[i][j]);
             }
         }
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int mead=-1;
-        if(n%2==0)
-        {
-            mead=nums[(n/2)-1];
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int mead = -1;
+        if (n % 2 == 0) {
+            mead = nums[(n / 2) - 1];
+        } else {
+            mead = nums[n / 2];
         }
-        else
-        {
-            mead=nums[n/2];
-        }
-        int a=nums[0];
-        int res=(mead-nums[0])/x;
-        for(int i=1;i<n;i++)
-        {
-            if((nums[i]-a)%x!=0)
-            {
+        int a = nums[0];
+        int res = (mead - nums[0]) / x;
+        for (int i = 1; i < n; i++) {
+            if ((nums[i] - a) % x != 0) {
                 return -1;
             }
-            res=res+abs(mead-nums[i])/x;
+            res = res + abs(mead - nums[i]) / x;
         }
         return res;
     }
