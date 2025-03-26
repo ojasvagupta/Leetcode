72275ec68e45305061062fe88ec4bc0915c86041
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-03-26 22:28:28 +0530

    Time: 55 ms (21.28%), Space: 90.2 MB (20.97%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -9,16 +9,10 @@ public:
         }
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        int mead = -1;
-        if (n % 2 == 0) {
-            mead = nums[(n / 2) - 1];
-        } else {
-            mead = nums[n / 2];
-        }
-        int a = nums[0];
+        int mead = nums[n/2];
         int res = 0;
         for (int i = 0; i < n; i++) {
-            if ((nums[i] - a) % x != 0) {
+            if ((nums[i] - nums[0]) % x != 0) {
                 return -1;
             }
             res = res + abs(mead - nums[i]) / x;
