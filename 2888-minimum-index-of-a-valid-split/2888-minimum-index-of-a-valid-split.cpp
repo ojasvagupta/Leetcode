ead4544405929181e513faba353c18958613b03c
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-03-27 20:00:35 +0530

    Time: 43 ms (44.67%), Space: 98.6 MB (56.56%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        int dom=INT_MIN;
-        int count=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(mp.count(nums[i]))
-            {
+        unordered_map<int, int> mp;
+        int dom = INT_MIN;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.count(nums[i])) {
                 mp[nums[i]]++;
-                if(mp[nums[i]]>count)
-                {
-                    count=mp[nums[i]];
-                    dom=nums[i];
+                if (mp[nums[i]] > count) {
+                    count = mp[nums[i]];
+                    dom = nums[i];
                 }
-            }
-            else
-            {
-                mp[nums[i]]=1;
+            } else {
+                mp[nums[i]] = 1;
             }
         }
-        count=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==dom)
-            {
+        count = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == dom) {
                 count++;
-                if(count>(i+1)/2 && mp[nums[i]]-count>(n-i-1)/2)
-                {
+                if (count > (i + 1) / 2 &&
+                    mp[nums[i]] - count > (n - i - 1) / 2) {
                     return i;
                 }
-
             }
         }
         return -1;
