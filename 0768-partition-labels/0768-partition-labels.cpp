699d0f300035135ed8e39d5119720586192f5bd8
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-03-30 17:18:55 +0530

    Time: 0 ms (100%), Space: 9.2 MB (15.55%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char,pair<int,int>> mp;
-        for(int i=0;i<s.size();i++)
-        {
-            if(mp.count(s[i]))
-            {
-                mp[s[i]].second=i;
-            }
-            else
-            {
-                mp[s[i]].first=i;
-                mp[s[i]].second=i;
+        unordered_map<char, pair<int, int>> mp;
+        for (int i = 0; i < s.size(); i++) {
+            if (mp.count(s[i])) {
+                mp[s[i]].second = i;
+            } else {
+                mp[s[i]].first = i;
+                mp[s[i]].second = i;
             }
         }
-        vector<pair<int,int>> v;
-        for(auto [x,y] : mp)
-        {
+        vector<pair<int, int>> v;
+        for (auto [x, y] : mp) {
             v.push_back(y);
         }
-        sort(v.begin(),v.end());
-        int start=v[0].first;
-        int end=v[0].second;
+        sort(v.begin(), v.end());
+        int start = v[0].first;
+        int end = v[0].second;
         vector<int> res;
-        for(int i=1;i<v.size();i++)
-        {
-            if(v[i].first>end)
-            {
-                res.push_back(end-start+1);
-                start=v[i].first;
-                end=v[i].second;
+        for (int i = 1; i < v.size(); i++) {
+            if (v[i].first > end) {
+                res.push_back(end - start + 1);
+                start = v[i].first;
+                end = v[i].second;
+            } else {
+                start = min(start, v[i].first);
+                end = max(end, v[i].second);
             }
-            else
-            {
-                start=min(start,v[i].first);
-                end=max(end,v[i].second);
-            }
-            if(i==v.size()-1)
-            {
-                res.push_back(end-start+1);
+            if (i == v.size() - 1) {
+                res.push_back(end - start + 1);
             }
         }
         return res;
