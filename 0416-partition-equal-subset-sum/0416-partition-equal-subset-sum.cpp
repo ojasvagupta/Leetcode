a4943a571607dc054cbc7517e78c38319fba2ed7
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-04-07 23:39:18 +0530

    Time: 53 ms (90.59%), Space: 13.4 MB (90.95%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,45 +1,20 @@
 class Solution {
 public:
-    bool clc(vector<int>& nums, int a, int sum,
-             vector<vector<int>>& memo) {
-        if (sum<0) {
-            return false;
-        }
-        if (sum==0) {
-            return true;
-        }
-        if (memo[a][sum] != -1) {
-            if (memo[a][sum] == 1) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        bool res = false;
-        for (int i = a; i < nums.size(); i++) {
-            res = res || clc(nums, i + 1, sum-nums[i], memo);
-            if (res) {
-                break;
-            }
-        }
-        if (res) {
-            memo[a][sum] = 1;
-        } else {
-            memo[a][sum] = 0;
-        }
-        return res;
-    }
     bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            sum = sum + nums[i];
-        }
-        if (sum % 2 != 0) {
-            return false;
-        }
-        vector<vector<int>> memo(nums.size() + 1, vector<int>(sum + 1, -1));
-        sum = sum / 2;
+        int totalSum = 0;
+        for (int num : nums) totalSum += num;
 
-        return clc(nums, 0,sum, memo);
+        if (totalSum % 2 != 0) return false;
+
+        int targetSum = totalSum / 2;
+        vector<bool> dp(targetSum + 1, false);
+        dp[0] = true;
+        for (int num : nums) {
+            for (int currSum = targetSum; currSum >= num; --currSum) {
+                dp[currSum] = dp[currSum] || dp[currSum - num];
+                if (dp[targetSum]) return true;
+            }
+        }
+        return dp[targetSum];
     }
 };
