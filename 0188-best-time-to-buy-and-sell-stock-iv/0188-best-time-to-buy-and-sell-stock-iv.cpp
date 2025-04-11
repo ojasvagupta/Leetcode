dc50468642c5b0e6630cdaff25cf358568ce0c04
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-04-11 23:04:24 +0530

    Time: 20 ms (17.93%), Space: 18.5 MB (17.14%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-   int solveMem(int index, int buy, vector<int> &prices,int limit, vector<vector<vector<int>>>& dp)
-    {
+    int solveMem(int index, int buy, vector<int>& prices, int limit,
+                 vector<vector<vector<int>>>& dp) {
         if (index == prices.size())
             return 0;
 
-        if(limit == 0) return 0;
+        if (limit == 0)
+            return 0;
 
-        if(dp[index][buy][limit] != -1) return dp[index][buy][limit];
+        if (dp[index][buy][limit] != -1)
+            return dp[index][buy][limit];
 
         int profit = 0;
 
-        if (buy)
-        {
-            int Buy = -prices[index] + solveMem(index + 1, 0, prices, limit,dp);
-            int skip = 0 + solveMem(index + 1, 1, prices, limit,dp);
+        if (buy) {
+            int Buy =
+                -prices[index] + solveMem(index + 1, 0, prices, limit, dp);
+            int skip = 0 + solveMem(index + 1, 1, prices, limit, dp);
             profit = max(Buy, skip);
-        }
-        else
-        {
-            int sell = prices[index] + solveMem(index + 1, 1, prices, limit - 1,dp);
-            int skip = 0 + solveMem(index + 1, 0, prices, limit,dp);
+        } else {
+            int sell =
+                prices[index] + solveMem(index + 1, 1, prices, limit - 1, dp);
+            int skip = 0 + solveMem(index + 1, 0, prices, limit, dp);
             profit = max(sell, skip);
         }
 
         dp[index][buy][limit] = profit;
         return dp[index][buy][limit];
     }
-    int maxProfit(int k,vector<int>& prices) {
+    int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, -1)));
-        return solveMem(0,1,prices,k,dp);
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(2, vector<int>(k + 1, -1)));
+        return solveMem(0, 1, prices, k, dp);
     }
 };
