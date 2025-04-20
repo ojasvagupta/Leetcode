98a2ffe7f37bb9e55f7ef230f89ca88ed64cdf3e
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-04-20 12:08:15 +0530

    Time: 198 ms (16.85%), Space: 250.5 MB (11.33%) - LeetHub

---
+++ 3831-find-x-value-of-array-i/3831-find-x-value-of-array-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        vector<vector<long long>> dp(nums.size(), vector<long long>(k*k, 0));
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] = nums[i] % k;
+        }
+        dp[0][nums[0]] = 1;
+        int a = nums[0];
+        vector<long long> res(k, 0);
+        res[nums[0]]++;
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j < k; j++) {
+                if(dp[i-1][j]!=0)
+                {
+                    dp[i][(j * nums[i])%k] =dp[i][(j*nums[i])%k] +dp[i - 1][j];
+                }
+            }
+            dp[i][nums[i]]++;
+            for (int j = 0; j < k; j++) {
+                res[j] += dp[i][j];
+            }
+        }
+        return res;
+    }
+};
