5f61792f2e6a3c943ecb7b69ffcb69979670db7f
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-06-28 14:53:57 +0530

    Time: 0 ms (100%), Space: 14.5 MB (10.14%) - LeetHub

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    static bool cmp(const pair<int,int>& a, const pair<int,int>& b) {
+    static bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
         return a.second < b.second; // sort by index
     }
 
     vector<int> maxSubsequence(vector<int>& nums, int k) {
         // Min-heap of (value, index) to keep k largest values
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
 
         for (int i = 0; i < nums.size(); i++) {
             pq.push({nums[i], i});
-            if (pq.size() > k) pq.pop();  // Maintain top-k largest values
+            if (pq.size() > k)
+                pq.pop(); // Maintain top-k largest values
         }
 
         vector<pair<int, int>> v;
