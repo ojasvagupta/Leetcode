470e75dfbfcce0bc673d0235d24c163a63c4f2a1
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-06-30 15:47:23 +0530

    Time: 10 ms (87.57%), Space: 36.1 MB (94.99%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int count1=1;
-        int count2=1;
+        sort(nums.begin(), nums.end());
+        int count1 = 1;
+        int count2 = 1;
         int num2;
-        int res=0;
-        int i=0;
-        int num1=nums[0];
-        while(i+1<nums.size() && nums[i]==nums[i+1])
-        {
+        int res = 0;
+        int i = 0;
+        int num1 = nums[0];
+        while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
             count1++;
             i++;
         }
-        while(i+1<nums.size())
-        {
+        while (i + 1 < nums.size()) {
             i++;
-            num2=nums[i];
-            while(i+1<nums.size() && nums[i]==nums[i+1])
-            {
+            num2 = nums[i];
+            while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
                 count2++;
                 i++;
-                
             }
-            if(num2-num1==1)
-            {
-                res=max(res,count1+count2);
+            if (num2 - num1 == 1) {
+                res = max(res, count1 + count2);
             }
-            count1=count2;
-            num1=num2;
-            count2=1;
+            count1 = count2;
+            num1 = num2;
+            count2 = 1;
         }
         return res;
     }
