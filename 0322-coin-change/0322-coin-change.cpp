2d3b3030454175a0ab18456e561a62f22a51f5f2
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-08-12 14:37:54 +0530

    Time: 19 ms (83.75%), Space: 17.9 MB (85.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,47 +1,18 @@
 class Solution {
 public:
-void numbers(vector<int>& coins, int amount,int temp,int& count,vector<int> & DP)
-{
-    if(DP[amount]<=temp)
-    {
-        return;
-    }
-    if(amount<0)
-    {
-        return;
-    }
-    if(amount==0)
-    {
-        count=min(count,temp);
-        return;
-    }
-    temp++;
-    for(int i=coins.size()-1;i>=0;i--)
-    {
-        if(coins[i]<=amount)
-        {
-            
-            int tempamount=amount-coins[i];
-            numbers(coins,tempamount,temp,count,DP);
-        }
-    }
-    temp--;
-    if(DP[amount]>temp)
-    {
-        DP[amount]=temp;
-    }
-    
-}
     int coinChange(vector<int>& coins, int amount) {
-        sort(coins.begin(),coins.end());
-        int count=INT_MAX;
-        vector<int> DP(amount+1,INT_MAX);
-        numbers(coins,amount,0,count,DP);
-        if(count==INT_MAX)
+        vector<int> memo(amount+1,INT_MAX-1);
+        memo[0]=0;
+        for(int i=1; i<=amount;i++)
         {
-            return -1;
+            for(auto a : coins)
+            {
+                if(i-a>=0)
+                {
+                    memo[i]=min(memo[i],memo[i-a]+1);
+                }
+            }
         }
-        return count;
-        
+        return (memo[amount] == INT_MAX - 1) ? -1 : memo[amount];
     }
 };
