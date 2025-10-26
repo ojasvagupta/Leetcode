a01a48f32db45ec10b4e42a73c534a5590af4f85
ojasvagupta <153262119+ojasvagupta@users.noreply.github.com>
2025-10-26 15:46:42 +0530

    Time: 7 ms (86.26%), Space: 123.7 MB (60.18%) - LeetHub

--- 2169-simple-bank-system/2169-simple-bank-system.cpp
+++ 2169-simple-bank-system/2169-simple-bank-system.cpp
@@ -1,36 +1,33 @@
 class Bank {
 public:
-vector<long long> v;
-int n;
+    vector<long long> v;
+    int n;
     Bank(vector<long long>& balance) {
-        v=balance;
-        n=balance.size();
+        v = balance;
+        n = balance.size();
     }
-    
+
     bool transfer(int account1, int account2, long long money) {
-        if(account1<=n && account2<=n && money<=v[account1-1])
-        {
-            v[account2-1]=v[account2-1]+money;
-            v[account1-1]=v[account1-1]-money;
+        if (account1 <= n && account2 <= n && money <= v[account1 - 1]) {
+            v[account2 - 1] = v[account2 - 1] + money;
+            v[account1 - 1] = v[account1 - 1] - money;
             return true;
         }
-        
+
         return false;
     }
-    
+
     bool deposit(int account, long long money) {
-        if(account<=n)
-        {
-            v[account-1]=v[account-1]+money;
+        if (account <= n) {
+            v[account - 1] = v[account - 1] + money;
             return true;
         }
         return false;
     }
-    
+
     bool withdraw(int account, long long money) {
-        if(account<=n && v[account-1]>=money)
-        {
-            v[account-1]=v[account-1]-money;
+        if (account <= n && v[account - 1] >= money) {
+            v[account - 1] = v[account - 1] - money;
             return true;
         }
 
